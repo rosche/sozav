644ebc469f1de68c494ee82d9e5e69f0835d90f3
roderick <roderick>
2008-07-18 18:55:26 +0000

    Formerly lib/Game/Util/Deck.pm.~2~

--- lib/Game/Util/Deck.pm
+++ lib/Game/Util/Deck.pm
@@ -1,4 +1,4 @@
-# $Id: Deck.pm,v 1.1 2008-07-17 17:40:57 roderick Exp $
+# $Id: Deck.pm,v 1.2 2008-07-18 18:55:26 roderick Exp $
 
 package Game::Util::Deck;
 
@@ -6,23 +6,18 @@ use strict;
 
 use base qw(Exporter);
 
-use RS::Handy	qw(badinvo create_constant_subs data_dump dstr xcroak);
+use Game::Util	qw(add_array_indices debug make_rw_accessor);
+use RS::Handy	qw(badinvo data_dump dstr xcroak);
 
 use vars qw($VERSION @EXPORT @EXPORT_OK);
 
-$VERSION = q$Revision: 1.1 $ =~ /(\d\S+)/ ? $1 : '?';
-
-BEGIN {
-    @EXPORT_OK = qw(
-    	@Energy_distribution
-    );
-}
+$VERSION = q$Revision: 1.2 $ =~ /(\d\S+)/ ? $1 : '?';
 
 use subs grep { /^[a-z]/    } @EXPORT, @EXPORT_OK;
 use vars grep { /^[\$\@\%]/ } @EXPORT, @EXPORT_OK;
 
 BEGIN {
-    add_array_indices 'DECK', qw(draw discard);
+    add_array_indices 'DECK', qw(DRAW DISCARD AUTO_RESHUFFLE);
 }
 
 sub new {
@@ -32,6 +27,7 @@ sub new {
     my $self = bless [], $class;
     $self->[DECK_DRAW]    = [];
     $self->[DECK_DISCARD] = [];
+    $self->a_auto_reshuffle(1);
 
     if (@card) {
 	$self->discard(@card);
@@ -40,6 +36,8 @@ sub new {
 
     return $self;
 }
+
+make_rw_accessor a_auto_reshuffle => DECK_AUTO_RESHUFFLE;
 
 sub shuffle {
     @_ == 1 || badinvo;
@@ -52,17 +50,19 @@ sub shuffle {
 }
 
 sub draw {
-    @_ == 1 || badinvo;
-    my ($self) = @_;
+    @_ == 1 || @_ == 2 || badinvo;
+    my $self = shift;
+    my $ct   = @_ ? shift : 1;
 
-    if (!@{ $self->[DECK_DRAW] }) {
-	$self->shuffle;
-	if (!@{ $self->[DECK_DRAW] }) {
-	    xcroak "ran out of $Gem[$self->[DECK_GTYPE]] cards";
+    my @r;
+    for (1..$ct) {
+	if (!@{ $self->[DECK_DRAW] } && $self->[DECK_AUTO_RESHUFFLE]) {
+	    $self->shuffle;
 	}
+	push @r, shift @{ $self->[DECK_DRAW] };
     }
 
-    return shift @{ $self->[DECK_DRAW] };
+    return @r == 1 ? $r[0] : @r;
 }
 
 sub discard {
@@ -73,5 +73,3 @@ sub discard {
 }
 
 1
-
-__END__
