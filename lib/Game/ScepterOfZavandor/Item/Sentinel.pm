bfd3c042ed1c3a46bd6f53882cf1244262e9a6f1
roderick <roderick>
2008-07-25 16:56:22 +0000

    Formerly lib/Game/ScepterOfZavandor/Item/Sentinel.pm.~2~

--- lib/Game/ScepterOfZavandor/Item/Sentinel.pm
+++ lib/Game/ScepterOfZavandor/Item/Sentinel.pm
@@ -1,4 +1,4 @@
-# $Id: Sentinel.pm,v 1.1 2008-07-25 16:33:12 roderick Exp $
+# $Id: Sentinel.pm,v 1.2 2008-07-25 16:56:22 roderick Exp $
 
 use strict;
 
@@ -10,39 +10,33 @@ use Game::Util	qw(add_array_index debug make_ro_accessor);
 use RS::Handy	qw(badinvo data_dump dstr shuffle xcroak);
 
 use Game::ScepterOfZavandor::Constant qw(
-    /^ARTI_/
     /^ITEM_/
+    /^SENT_/
     @Sentinel
+    @Sentinel_real_ix_xxx
     @Sentinel_data
 );
-
-#BEGIN {
-#    add_array_index 'ITEM_AUC', $_ for map { "ARTI_$_" } qw(TYPE);
-#}
 
 sub new {
     @_ == 2 || badinvo;
     my ($class, $auc_type) = @_;
 
-    #XXX
-    #defined $Sentinel[$auc_type] or xcroak;;
-
-    my $self = $class->SUPER::new(ITEM_TYPE_SENTINEL, $auc_type, \@Sentinel_data);
-
-    $self->a_vp($self->data(ARTI_DATA_VP));
-    $self->a_gem_slots($self->data(ARTI_DATA_GEM_SLOTS));
-    $self->a_hand_limit_modifier($self->data(ARTI_DATA_HAND_LIMIT));
+    my $self = $class->SUPER::new(ITEM_TYPE_SENTINEL,
+				    $auc_type, \@Sentinel_data);
 
     return $self;
 }
 
-sub as_string_fields {
-    @_ || badinvo;
+# XXX name
+sub new_deck {
+    @_ == 1 || badinvo;
     my $self = shift;
-    my @r = $self->SUPER::as_string_fields(@_);
-    push @r,
-	$self->data(ARTI_DATA_DECK_LETTER, ARTI_DATA_NAME);
-    return @r;
+
+    my @a = ();
+    for (@Sentinel_real_ix_xxx) {
+	push @a, __PACKAGE__->new($_);
+    }
+    return @a;
 }
 
 1
