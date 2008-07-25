1e90415074b724473ccd05cf5841c1a164433c1e
roderick <roderick>
2008-07-25 17:41:18 +0000

    Formerly lib/Game/ScepterOfZavandor/Item/Auctionable.pm.~3~

--- lib/Game/ScepterOfZavandor/Item/Auctionable.pm
+++ lib/Game/ScepterOfZavandor/Item/Auctionable.pm
@@ -1,4 +1,4 @@
-# $Id: Auctionable.pm,v 1.2 2008-07-25 01:05:45 roderick Exp $
+# $Id: Auctionable.pm,v 1.3 2008-07-25 17:41:18 roderick Exp $
 
 use strict;
 
@@ -7,17 +7,18 @@ package Game::ScepterOfZavandor::Item::Auctionable;
 use base qw(Exporter Game::ScepterOfZavandor::Item);
 
 use Carp	qw(confess);
-use Game::Util	qw(add_array_index debug make_ro_accessor);
+use Game::Util	qw(add_array_index debug make_ro_accessor make_rw_accessor);
 use RS::Handy	qw(badinvo data_dump dstr xconfess);
 
 use Game::ScepterOfZavandor::Constant qw(
     /^ITEM_/
     /^AUC_/
+    @Auctionable_data_field
 );
 
 use vars qw($VERSION @EXPORT @EXPORT_OK);
 BEGIN {
-    $VERSION = q$Revision: 1.2 $ =~ /(\d\S+)/ ? $1 : '?';
+    $VERSION = q$Revision: 1.3 $ =~ /(\d\S+)/ ? $1 : '?';
     @EXPORT_OK = qw(
 	auc_type_is_artifact
 	auc_type_is_sentinel
@@ -27,7 +28,7 @@ use subs grep { /^[a-z]/    } @EXPORT, @EXPORT_OK;
 use vars grep { /^[\$\@\%]/ } @EXPORT, @EXPORT_OK;
 
 BEGIN {
-    add_array_index 'ITEM', $_ for map { "AUC_$_" } qw(TYPE MIN_BID);
+    add_array_index 'ITEM', $_ for map { "AUC_$_" } qw(TYPE PLAYER DATA);
 }
 
 #sub auc_type_is_artifact {
@@ -47,29 +48,64 @@ sub new {
     @_ == 4 || badinvo;
     my ($class, $itype, $auc_type, $rauc_data) = @_;
 
-    # XXX validate $auc_type
+    $rauc_data->[$auc_type]
+	or xconfess $auc_type;
 
     my $self = $class->SUPER::new($itype);
     $self->[ITEM_AUC_TYPE] = $auc_type;
-    $self->[ITEM_AUC_MIN_BID] = $rauc_data->[$auc_type][AUC_DATA_MIN_BID];
-    $self->a_vp($rauc_data->[$auc_type][AUC_DATA_VP]);
+    $self->[ITEM_AUC_DATA] = $rauc_data->[$auc_type];
+    $self->a_vp($self->data(AUC_DATA_VP));
 
     return $self;
 }
 
 make_ro_accessor (
     a_auc_type => ITEM_AUC_TYPE,
-    a_min_bid  => ITEM_AUC_MIN_BID,
 );
+
+make_rw_accessor (
+    a_player => ITEM_AUC_PLAYER,
+);
+
+for my $i (0..$#Auctionable_data_field) {
+    no strict 'refs';
+    *{ "get_" . lc $Auctionable_data_field[$i] } = sub {
+	@_ == 1 || badinvo;
+	return $_[0]->data($i);
+    }
+}
 
 sub as_string_fields {
     @_ || badinvo;
     my $self = shift;
     my @r = $self->SUPER::as_string_fields(@_);
     push @r,
-    	# XXX name
-	sprintf("min=%3d", $self->a_min_bid);
+	$self->data(AUC_DATA_NAME),
+	sprintf("min=%3d", $self->get_min_bid);
     return @r;
+}
+
+sub data {
+    @_ >= 2 || badinvo;
+    my $self = shift;
+    my @ix   = @_;
+
+    my $rd = $self->[ITEM_AUC_DATA];
+    my @r;
+    for my $ix (@ix) {
+	$ix >= 0 && $ix <= $#{ $rd } || xconfess dstr $ix;
+	push @r, $rd->[$ix];
+    }
+
+    return @r == 1 ? $r[0] : @r;
+}
+
+
+sub free_items {
+}
+
+sub own_only_one {
+    return 0;
 }
 
 1
