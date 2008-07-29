1bf5f85881ee77ce93a877fc7c4c7a1eee675d6b
roderick <roderick>
2008-07-29 18:16:32 +0000

    Formerly lib/Game/ScepterOfZavandor/Game.pm.~6~

--- lib/Game/ScepterOfZavandor/Game.pm
+++ lib/Game/ScepterOfZavandor/Game.pm
@@ -1,13 +1,14 @@
-# $Id: Game.pm,v 1.5 2008-07-25 17:30:40 roderick Exp $
+# $Id: Game.pm,v 1.6 2008-07-29 18:16:32 roderick Exp $
 
 use strict;
 
 package Game::ScepterOfZavandor::Game;
 
-# XXX class::makemethods
+# XXY class::makemethods
 
-use Game::Util	qw(add_array_indices debug debug_var make_ro_accessor);
-use RS::Handy	qw(badinvo create_constant_subs data_dump dstr xconfess);
+use Game::Util	qw(add_array_indices debug debug_var info make_ro_accessor
+		    make_rw_accessor);
+use RS::Handy	qw(badinvo create_constant_subs data_dump dstr shuffle xconfess);
 use Scalar::Util qw(refaddr);
 
 use Game::ScepterOfZavandor::Constant	qw(
@@ -17,12 +18,15 @@ use Game::ScepterOfZavandor::Constant	qw(
     @Config_by_num_players
     @Dust_data
     $Dust_data_val_1
+    $Game_end_sentinels_sold_count
     @Gem
     @Option
     %Option
+    @Sentinel_real_ix_xxx
 );
 use Game::ScepterOfZavandor::Item::Artifact	();
 use Game::ScepterOfZavandor::Item::Sentinel	();
+use Game::ScepterOfZavandor::Item::TurnOrder	();
 use Game::ScepterOfZavandor::Deck		();
 use Game::ScepterOfZavandor::Player		();
 
@@ -36,8 +40,8 @@ BEGIN {
 	'ARTIFACTS_ON_AUCTION',
 	'ARTIFACTS_AT_ONCE',
 	'SENTINEL',
+	'TURN_ORDER',
 	'TURN_NUM',
-	'PLAYER_ORDER',
     );
 }
 
@@ -61,6 +65,10 @@ sub new {
 
 make_ro_accessor (
     a_gem_decks => GAME_GEM_DECKS,
+);
+
+make_rw_accessor (
+    a_turn_num => GAME_TURN_NUM,
 );
 
 sub die_if_initialized {
@@ -108,10 +116,8 @@ sub init {
 
     $self->die_if_initialized;
 
-    # XXY
-    print "options: ", join(" ",
-	map { ($self->option($_) ? "" : "!") . $Option[$_] } 0..$#Option),
-	"\n";
+    info "options: ", join(" ",
+	map { ($self->option($_) ? "" : "!") . $Option[$_] } 0..$#Option);
 
     my $num_players = $self->num_players;
     debug_var num_players => $num_players;
@@ -146,12 +152,24 @@ sub init {
     $self->[GAME_SENTINEL]
 	= [Game::ScepterOfZavandor::Item::Sentinel->new_deck];
 
+    # create turn order markers
+
+    $self->[GAME_TURN_ORDER] = [];
+    for (0 .. $num_players-1) {
+    	push @{ $self->[GAME_TURN_ORDER] },
+	    Game::ScepterOfZavandor::Item::TurnOrder->new(undef, $_);
+    }
+
     # assign characters and initialize players
 
     my @c = RS::Handy::shuffle 0..$#Character;
     for my $player ($self->players) {
     	$player->init(shift @c);
     }
+
+    # XXX convenience when testing
+    $self->[GAME_PLAYER] =
+	[sort { $a->a_char <=> $b->a_char } $self->players];
 
     $self->[GAME_TURN_NUM]    = 0;
     $self->[GAME_INITIALIZED] = 1;
@@ -166,40 +184,72 @@ sub play {
     while (1) {
     	# phase 1. turn order
 
-	# XXX
+	$self->a_turn_num(1 + $self->a_turn_num);
 
-	$self->[GAME_PLAYER_ORDER] = [$self->players];
+    	my @p = $self->players_in_order;
+	if ($self->a_turn_num > 1) {
+	    for (@p) {
+		# XXX unset player?  do this from remove_items?
+		$_->remove_items($_->turn_order_card);
+	    }
+	}
+	for (0..$#p) {
+	    my $to = $self->[GAME_TURN_ORDER][$_];
+	    $to->a_player($p[$_]);
+	    $p[$_]->add_items($to);
+	    $p[$_]->a_score_at_turn_start($p[$_]->score);
+	}
 
 	# phase 1. refill artifacts
 
-	while (@{ $self->[GAME_ARTIFACTS_ON_AUCTION] } < $self->[GAME_ARTIFACTS_AT_ONCE]) {
+	while (@{ $self->[GAME_ARTIFACTS_ON_AUCTION] }
+		    < $self->[GAME_ARTIFACTS_AT_ONCE]) {
 	    my $i = $self->[GAME_ARTIFACT_DECK]->draw
 		or last;
-	    # XXX info output
-	    # XXX more details, cost, discounts, vp, effect
-	    print "New artifact on auction: $i\n";
 	    push @{ $self->[GAME_ARTIFACTS_ON_AUCTION] }, $i;
 	}
 
 	# phase 2. gain energy
 
-	for ($self->players) {
+	for (@p) {
 	    $_->gain_energy;
 	}
 
 	# phase 3: player actions
 
-	for (@{ $self->[GAME_PLAYER_ORDER] }) {
+	for (@p) {
 	    $_->actions;
 	}
 
 	# phase 4: check victory conditions
 
+	if (@Sentinel_real_ix_xxx - @{ $self->[GAME_SENTINEL] }
+	    	>= $Game_end_sentinels_sold_count) {
+	    last;
+	}
+
 	# phase 4: hand limit
 
-	for ($self->players) {
+	for (@p) {
 	    $_->enforce_hand_limit;
 	}
+    }
+
+    info "Game over";
+    my $place         = 0;
+    my $nominal_place = 0;
+    my $prev_score    = undef;
+    for ($self->players_in_order) {
+	$nominal_place++;
+    	my $this_score = $_->score;
+	$place = (defined $prev_score && $this_score == $prev_score)
+    	    	    	? $place
+			: $nominal_place;
+	info sprintf "  %s. %3d %s",
+	    $place,
+	    $_->score,
+	    $_->name;
+	$prev_score = $this_score;
     }
 }
 
@@ -253,6 +303,22 @@ sub players {
     my ($self) = @_;
 
     return @{ $self->[GAME_PLAYER] };
+}
+
+sub players_in_order {
+    @_ == 1 || badinvo;
+    my ($self) = @_;
+
+    # Arbitrary order for players with the same score and score from
+    # gems is done by doing an initial shuffle and using a stable sort.
+
+    my @p = shuffle $self->players;
+
+    return map { $p[$_] } sort { 0
+    	    or $p[$b]->score           <=> $p[$a]->score
+    	    or $p[$b]->score_from_gems <=> $p[$a]->score_from_gems
+    	    or    $b                   <=>    $a
+    } 0..$#p;
 }
 
 sub num_players {
